1571892f886118fca67c2b00b9db9cac3cfd4c1d
LeonardoDelgado1905 <sdelgadom@unal.edu.co>
2024-12-07 15:21:40 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+import joblib
+
+# Reemplace esto con su implementación:
+class ApiInput(BaseModel):
+    features: List[float]
+
+# Reemplace esto con su implementación:
+class ApiOutput(BaseModel):
+    forecast : float
+
+app = FastAPI()
+model = joblib.load("model.joblib")
+
+# Reemplace esto con su implementación:
+@app.post("/predict")
+async def predict(data: ApiInput) -> ApiOutput:
+    return ApiOutput(forecast=model.predict([data.features]))
